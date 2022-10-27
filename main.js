072c2ce764d7c8d731d900842f2eaa6016a9c97d
juliaCCC <105754513+juliaCCC@users.noreply.github.com>
2022-10-27 19:54:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name = "Ranbin";
+var last_name = "Kapoor";
+var fullName = first_name.concat(last_name);
+
+var a = 10;
+var b = 5;
+
+console.log(fullName);
+var result = a*b;
+console.log(result);
